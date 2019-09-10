46d05b262fce52c48d16c1a4485aa1ac43f5d566
swamay17 <47160118+swamay17@users.noreply.github.com>
2019-09-10 11:21:41 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?/php>
+database=
+username=
+password=
+host=
+?>
+#ctrl+shift + x
+
+nano.gitignore
+#inside
+confi.php ( or config.ini.whichever you created)
+#commit this file
+nano.gitignore (again)
+conf.php
+commit.gitignore file
+nano conf.php
+<?php
+database=
+username=
+password=
+
+
+?/
+#try to commit conf.php (git ignore should work for this file since its not tracked)
