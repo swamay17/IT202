ce6d1cfe3db60bee17f492c5d49878303bbc6b5c
swamay17 <47160118+swamay17@users.noreply.github.com>
2019-09-12 10:43:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,11 @@ database=
 username=
 password=
 
+  #this is in the index.php
+  <?php
+  echo "hello world";
+?>
+
 
 ?/
 #try to commit conf.php (git ignore should work for this file since its not tracked)
