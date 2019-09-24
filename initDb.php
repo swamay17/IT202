82c9ea9de8e77dc43a7d6d7945941c30c9d9bc33
swamay17 <47160118+swamay17@users.noreply.github.com>
2019-09-24 10:38:22 -0400

    Update initDb.php

--- initDb.php
+++ initDb.php
@@ -1,3 +1,4 @@
+
 <?php
 #turn error reporting on
 ini_set('display_errors',1);
@@ -5,11 +6,29 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //pull in config.php so we can access the variables from it
 require('config.php');
-echo "Loaded Host: " . $host;
+//echo "Loaded Host: " . $host;
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
+	foreach(glob("sql/*.sql") as $filename){
+		//echo $filename;
+		$sql[$filename] = file_get_contents($filename);
+		//echo $sql[$filename];
+	}
+	ksort($sql);
+	//connect to DB
 	$db = new PDO($conn_string, $username, $password);
-	echo "Connected";
+	//$db->setAttribute(PDO::ATTR_ERRMODE);
+	foreach($sql as $key => $value){
+		echo "<br>Running: " . $key;
+		$stmt = $db->prepare($value);
+		$result = $stmt->execute();
+		$error = $stmt->errorInfo();
+		if($error && $error[0] !== '00000'){
+			echo "<br>Error:<pre>" . var_export($error,true) . "</pre><br>";
+		}
+		echo "<br>$key result: " . ($result>0?"Success":"Fail") . "<br>";
+	}
+	
 }
 catch(Exception $e){
 	echo $e->getMessage();
